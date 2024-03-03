7841404d4ce2f9290f3a8406c7c6ae47f3591aad
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2024-03-02 23:27:09 -0500

    Aprendido
    Teclado permanente hasta que lo modifique, sino se mantiene como antes

--- comands/start.js
+++ comands/start.js
@@ -1,11 +1,30 @@
 const { bot } = require("..");
-const { replyMarkupPrincipal } = require("../constants");
+const { BUTTONS } = require("../utils/buttons");
+// let replyMarkup = bot.keyboard([[BUTTONS.menu.label], [BUTTONS.hide.label]], {
+//   resize: true,
+// });
+
 function start() {
   bot.on(["/start", "/iniciar"], (msg) => {
+    let replyMarkup = bot.keyboard(
+      // Manda a llamar un comando en este caso el del menu
+      // ✔️ Listo ya el menu
+      [
+        [BUTTONS.menu.label], //❌ Al lado de este boton debe ir el boton de panel de administracion que solo se ve para el administrador
+
+        // Manda a subscribirse en la tienda
+        // ❌ Falta por implementar esta funcionalidad
+        [BUTTONS.regist.label],
+      ],
+
+      {
+        resize: true,
+      }
+    );
     return bot.sendMessage(
       msg.from.id,
       "Bienvenido a la Cafetería el Enano, este es el asistente bot listo para servirle!.",
-      replyMarkupPrincipal(bot)
+      { replyMarkup }
     );
   });
 }
